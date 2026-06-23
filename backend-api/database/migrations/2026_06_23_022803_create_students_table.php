33673aeea63ca11fc0d2173eba0af52f852b8f73
Leonardo <alfianleonardo2@gmail.com>
2026-06-23 14:29:04 +0700

    First Commit

---
+++ backend-api/database/migrations/2026_06_23_022803_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('nis')->unique();
+            $table->string('nama');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->string('alamat')->nullable();
+            $table->string('no_hp')->nullable();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
